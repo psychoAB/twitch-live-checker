fddc4645654d18e4d6af998b577878dc56226aec
psychoAB <natchanan.th@outlook.com>
2021-06-06 13:13:08 +0700

    tried to refactor

--- twitch-live-checker.py
+++ twitch-live-checker.py
@@ -10,80 +10,107 @@ retry_limit = 5
 
 #================================================
 
-if len( sys.argv ) > 1:
-    filepath = sys.argv[ 1 ]
+def main():
 
-#================================================
+    global filepath;
 
-try:
-    file = open( filepath, 'r' )
-except FileNotFoundError as error:
-    print( str( error ) )
+    if len( sys.argv ) > 1:
+        filepath = sys.argv[ 1 ]
 
-    quit( error.errno )
-except Exception as error:
-    print( str( error ) )
-
-    print( "It's unexpected." )
-
-    quit( error.errno )
-
-file_text = file.read()
-
-file.close()
-
-#================================================
-
-streamer_list = file_text.split( '\n' )
-
-streamer_list = streamer_list[ 0 : -1 ]
-
-#================================================
-
-streamer_max_length = max( map( len, streamer_list ) )
-
-#================================================
-
-for streamer in streamer_list:
-
-    should_retry = True;
-    retry_count = 0
-
-    while should_retry == True and retry_count < retry_limit:
-
-        try:
-            html_content = urllib.request.urlopen( 'https://www.twitch.tv/' + streamer ).read().decode( 'utf-8' )
-        except urllib.error.URLError as error:
-            if type( error.reason ) == socket.gaierror:
-                print( str( error.reason ) )
-
-                print( 'Check your network connection.' )
-
-                quit( error.reason.errno )
+    file_text = read_streamer_list_file( filepath )
+    
+    streamer_list = parse_streamer_list_file( file_text )
+    
+    streamer_max_length = max( map( len, streamer_list ) )
+    
+    for streamer in streamer_list:
+    
+        should_retry = True;
+        retry_count = 0
+    
+        while should_retry == True and retry_count < retry_limit:
+    
+            html_content = get_streamer_html_content( streamer )
+    
+            if html_content.find( 'isLiveBroadcast' ) != -1:
+                print_streamer_status( streamer, 'Live', streamer_max_length )
+    
+                should_retry = False
             else:
-                print( str( error.reason ) )
+                if html_content.find( streamer ) != -1:
+                    print_streamer_status( streamer, 'Offline', streamer_max_length )
+    
+                    should_retry = False
+                else:
+                    retry_count = retry_count + 1
+    
+        if retry_count >= retry_limit:
+            print_streamer_status( streamer, 'Not Found', streamer_max_length )
 
-                print( "It's unexpected." )
+#================================================
 
-                quit( error.errno )
-        except Exception as error:
-            print( str( error ) )
+def print_streamer_status( streamer, status, streamer_max_length ):
+    print( '{}\t'.format( streamer.ljust( streamer_max_length ) ) + status )
+
+#================================================
+
+def get_streamer_html_content( streamer ):
+    try:
+        html_content = urllib.request.urlopen( 'https://www.twitch.tv/' + streamer ).read().decode( 'utf-8' )
+    except urllib.error.URLError as error:
+        if type( error.reason ) == socket.gaierror:
+            print( str( error.reason ) )
+
+            print( 'Check your network connection.' )
+
+            quit( error.reason.errno )
+        else:
+            print( str( error.reason ) )
 
             print( "It's unexpected." )
 
             quit( error.errno )
+    except Exception as error:
+        print( str( error ) )
 
-        if html_content.find( 'isLiveBroadcast' ) != -1:
-            print( '{}\tLive'.format( streamer.ljust( streamer_max_length ) ) )
+        print( "It's unexpected." )
 
-            should_retry = False
-        else:
-            if html_content.find( streamer ) != -1:
-                print( '{}\tOffline'.format( streamer.ljust( streamer_max_length ) ) )
+        quit( error.errno )
 
-                should_retry = False
-            else:
-                retry_count = retry_count + 1
+    return html_content
 
-    if retry_count >= retry_limit:
-        print( '{}\tNot Found'.format( streamer.ljust( streamer_max_length ) ) )
+#================================================
+
+def parse_streamer_list_file( file_text ):
+    streamer_list = file_text.split( '\n' )
+
+    streamer_list = streamer_list[ 0 : -1 ]
+
+    return streamer_list
+
+#================================================
+
+def read_streamer_list_file( filepath ):
+    try:
+        file = open( filepath, 'r' )
+    except FileNotFoundError as error:
+        print( str( error ) )
+    
+        quit( error.errno )
+    except Exception as error:
+        print( str( error ) )
+    
+        print( "It's unexpected." )
+    
+        quit( error.errno )
+    
+    file_text = file.read()
+    
+    file.close()
+
+    return file_text
+
+#================================================
+
+if __name__ == '__main__':
+    main()
